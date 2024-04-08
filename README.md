b9f67dbff267faf8cf0757bcff9cc9305865c431
Scott A. Idler <scott.a.idler@gmail.com>
2024-04-08 09:51:12 -0700

    initial setup of brand new lazyvim starter setup

--- README.md
+++ README.md
@@ -1,4 +1,1 @@
-# nvim# 💤 LazyVim
-
-A starter template for [LazyVim](https://github.com/LazyVim/LazyVim).
-Refer to the [documentation](https://lazyvim.github.io/installation) to get started.
+# Scott Idler Neovim Config
--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,27 @@
--- Autocmds are automatically loaded on the VeryLazy event
--- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
--- Add any additional autocmds here
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+
+-- Set up the auto command to run the function when Neovim starts
+vim.api.nvim_create_autocmd("VimEnter", {
+    pattern = "*",
+    callback = function ()
+      local args = vim.fn.argv()
+      local num_args = #args
+
+      if num_args == 2 or num_args == 3 then
+        vim.cmd('edit ' .. args[1])  -- Open the first file normally
+        for i = 2, num_args do
+          vim.cmd('vsplit ' .. args[i])  -- Open subsequent files in vertical splits
+        end
+      end
+    end,
+})

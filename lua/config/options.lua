9023255952d562bd3214e1fbbbf7c8233d85e4ee
Scott A. Idler <scott.a.idler@gmail.com>
2024-04-10 08:42:49 -0700

    added some tabstop, expandtab, shiftwidth commands until I can get tpope/vim-sleuth working

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,6 +4,20 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+
+-- Set tabs to 2 spaces
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.expandtab = true
+
+-- Enable auto indenting and set it to spaces
+vim.opt.smartindent = true
+vim.opt.shiftwidth = 2
+
+-- Enable smart indenting (see https://stackoverflow.com/questions/1204149/smart-wrap-in-vim)
+vim.opt.breakindent = true
 
 -- Set to true if you have a Nerd Font installed
 vim.g.have_nerd_font = false

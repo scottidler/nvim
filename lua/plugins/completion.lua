3ad6ee0649e573379bbecb3be1c082e25a0a9af0
Scott A. Idler <scott.a.idler@gmail.com>
2025-02-15 15:19:48 -0800

    added nvim-cmp; saved latest state

---
+++ lua/plugins/completion.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    "hrsh7th/nvim-cmp",
+    enabled = true,
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-cmdline",
+    },
+    config = function()
+      local cmp = require("cmp")
+      cmp.setup({
+        mapping = cmp.mapping.preset.insert(),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "buffer" },
+          { name = "path" },
+        }),
+      })
+    end,
+  },
+}

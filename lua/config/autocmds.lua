24e4dd56b92622094412684f74439a2a52baca6d
Scott A. Idler <scott.a.idler@gmail.com>
2025-03-15 14:12:23 -0700

    added remove trailing whitespace to autocmds

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,30 +1,34 @@
 -- lua/config/autocmds.lua
 
-
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
 --  See `:help vim.highlight.on_yank()`
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlight when yanking (copying) text",
+  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
   callback = function()
     vim.highlight.on_yank()
   end,
 })
 
-
 -- Set up the auto command to run the function when Neovim starts
 vim.api.nvim_create_autocmd("VimEnter", {
-    pattern = "*",
-    callback = function ()
-      local args = vim.fn.argv()
-      local num_args = #args
+  pattern = "*",
+  callback = function()
+    local args = vim.fn.argv()
+    local num_args = #args
 
-      if num_args == 2 or num_args == 3 then
-        vim.cmd('edit ' .. args[1])  -- Open the first file normally
-        for i = 2, num_args do
-          vim.cmd('vsplit ' .. args[i])  -- Open subsequent files in vertical splits
-        end
+    if num_args == 2 or num_args == 3 then
+      vim.cmd("edit " .. args[1]) -- Open the first file normally
+      for i = 2, num_args do
+        vim.cmd("vsplit " .. args[i]) -- Open subsequent files in vertical splits
       end
-    end,
+    end
+  end,
+})
+
+-- Remove trailing whitespace on save
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*",
+  command = [[%s/\s\+$//e]],
 })

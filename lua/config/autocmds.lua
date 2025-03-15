361dc81b5906e7f85d8a1620f1c67a3ab87a20a2
Scott A. Idler <scott.a.idler@gmail.com>
2025-03-15 14:32:56 -0700

    added trim pluggin and removed autocmd trim function

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -26,9 +26,3 @@ vim.api.nvim_create_autocmd("VimEnter", {
     end
   end,
 })
-
--- Remove trailing whitespace on save
-vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = "*",
-  command = [[%s/\s\+$//e]],
-})

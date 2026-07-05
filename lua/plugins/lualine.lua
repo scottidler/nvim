401766f856327269daee61d87cf1841f53f1d5d6
Scott A. Idler <scott.a.idler@gmail.com>
2026-07-04 22:49:49 -0700

    fix lualine theme name (catppuccin -> catppuccin-macchiato)

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -44,7 +44,7 @@ return {
 
 			require("lualine").setup({
 				options = {
-					theme = "catppuccin",
+					theme = "catppuccin-macchiato",
 					globalstatus = true,
 					component_separators = { left = "", right = "" },
 					section_separators = { left = "█", right = "█" },
